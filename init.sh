346f03a32b86b700680cb9603a8db73d9f6e4dff
Cristian Morales <cristian.morales@makingscience.com>
2021-07-11 18:09:29 +0200

    Automating start of infrastrucutre

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 gcloud container clusters get-credentials projecto-demo-290916-gke --zone europe-west1-b --project projecto-demo-290916
 kubectx gke_projecto-demo-290916_europe-west1-b_projecto-demo-290916-gke
+istioctl install --set profile=demo -y
