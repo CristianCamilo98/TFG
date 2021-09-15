983a0008a88794d83279ca9f6aa6c04568d463e6
Cristian Morales <cristian.morales@makingscience.com>
2021-09-15 20:25:53 +0200

    Adding extra configuration to set up the cluster and to initialize Istio

--- init.sh
+++ init.sh
@@ -2,3 +2,5 @@
 gcloud container clusters get-credentials projecto-demo-290916-gke --zone europe-west1-b --project projecto-demo-290916
 kubectx gke_projecto-demo-290916_europe-west1-b_projecto-demo-290916-gke
 istioctl install --set profile=demo -y
+istioctl verify-instal
+kubectl apply -f istio-1.10.2/samples/addons/
