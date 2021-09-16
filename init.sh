5702341a16c5c7736921528108bfa7e29ac060a3
Cristian Camilo Morales Tapias <crismorales1998@hotmail.com>
2021-09-16 22:11:12 +0200

    Minor changes

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-gcloud config set account criscamota199890@gmail.com
 gcloud container clusters get-credentials projecto-demo-290916-gke --zone europe-west1-b --project projecto-demo-290916
 kubectx gke_projecto-demo-290916_europe-west1-b_projecto-demo-290916-gke
 istioctl install --set profile=demo -y
--- install_istio.sh
+++ install_istio.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 curl -L https://istio.io/downloadIstio | ISTIO_VERSION=1.10.2 sh -
