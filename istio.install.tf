f802742e12a4e11575e874ae69eb9b317bc2aff7
Cristian Camilo Morales Tapias <crismorales1998@hotmail.com>
2021-09-25 11:40:06 +0200

    Giving some time for cluster to be created completelly otherwise when trying to create namespace, timeout error will pop up

--- istio.install.tf
+++ istio.install.tf
@@ -8,6 +8,7 @@ resource "null_resource" "install_istio" {
     gcloud container clusters get-credentials projecto-demo-290916-gke --zone europe-west1-b --project projecto-demo-290916
     kubectx gke_projecto-demo-290916_europe-west1-b_projecto-demo-290916-gke
     istioctl install --set profile=demo -y
+    sleep 10
     EOT
   }
 
