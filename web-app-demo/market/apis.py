885d3abf993c053aa56f52ebaf7ec4acfea58451
Cristian Camilo Morales Tapias <crismorales1998@hotmail.com>
2021-10-29 17:05:22 +0200

    Never rename an python environemnt

--- web-app-demo/market/apis.py
+++ web-app-demo/market/apis.py
@@ -1,3 +1,8 @@
 from market import app
+from market  import api
 
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
 
+api.add_resource(HelloWorld,"/helloworld")
