41a60cf9a14bf75a2e279d355bbea0276e11c219
Cristian Camilo Morales Tapias <crismorales1998@hotmail.com>
2021-12-06 19:21:12 +0100

    Adding the installation of flagger

--- init.sh
+++ init.sh
@@ -26,3 +26,7 @@ kubens istioinaction
 
 # In order to deplay the dashboards of the addons run:
 # istioctl dashboard (jaeger|grafana|kiali|prometheus)
+# Installing Flagger into the cluster
+helm repo add flagger https://flagger.app
+kubectl apply -f https://raw.githubusercontent.com/fluxcd/flagger/main/artifacts/flagger/crd.yaml
+helm install flagger flagger/flagger --namespace=istio-system --set crd.create=false --set meshProvider=istio --set metricsServer=http://prometheus:9090
