d45fbdc6f0997ab0c05e710e1defcd8b037711cf
Cristian Camilo Morales Tapias <crismorales1998@hotmail.com>
2021-12-25 19:12:21 +0100

    Blocking communication from services to outside of the cluster

--- istio.install.tf
+++ istio.install.tf
@@ -7,7 +7,7 @@ resource "null_resource" "install_istio" {
     command = <<EOT
     gcloud container clusters get-credentials projecto-demo-290916-gke --zone europe-west1-b --project projecto-demo-290916
     kubectx gke_projecto-demo-290916_europe-west1-b_projecto-demo-290916-gke
-    istioctl install --set profile=demo -y
+    istioctl install --set profile=demo --set meshConfig.outboundTrafficPolicy.mode=REGISTRY_ONLY -y
     sleep 10
     EOT
   }
